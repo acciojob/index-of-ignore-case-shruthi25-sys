91c9b8d4e4ad1ab50fdef07633351fcbe92b0202
Suruthika Ramesh <manimala1614@gmail.com>
2026-07-07 22:00:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,6 @@ return s1.toLowerCase().indexOf(s2.toLowerCase());
 }
 
 // Please do not change the code below
-//const s1 = prompt("Enter s1:");
-//const s2 = prompt("Enter s2:");
+const s1 = prompt("Enter s1:");
+const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
